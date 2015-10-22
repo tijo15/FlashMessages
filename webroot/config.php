751e9ef293c442b3558306fe044bbb06cd716d0a
tijo15 <inf_576@hotmail.com>
2015-10-22 20:38:41 +0200

    Added files

--- webroot/config.php
+++ webroot/config.php
@@ -1,31 +1,13 @@
 <?php
 /**
- * Sample configuration file for Anax webroot.
+ * Get all configuration details to be able to execute the test suite.
  *
  */
-
-
+include __DIR__ . "/../autoloader.php";
+include __DIR__ . "/../vendor/autoload.php";
 /**
  * Define essential Anax paths, end with /
  *
  */
-define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../') . '/');
+define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../vendor/anax/mvc') . '/');
 define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . 'app/');
-
-
-
-/**
- * Include autoloader.
- *
- */
-include(ANAX_APP_PATH . 'config/autoloader.php'); 
-
-
-
-/**
- * Include global functions.
- *
- */
-include(ANAX_INSTALL_PATH . 'src/functions.php'); 
-
-
